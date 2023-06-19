7bc799b35881a4b9e374b2695de158989697f4f3
Dennis Li <denli@adobe.com>
2023-06-19 14:46:06 -0700

    Hello block

---
+++ first-bulk-publish.sh
@@ -0,0 +1,18 @@
+curl -X POST "https://admin.hlx.page/preview/dli3/denli/main/index" &&
+curl -X POST "https://admin.hlx.page/preview/dli3/denli/main/placeholders.json" &&
+curl -X POST "https://admin.hlx.page/preview/dli3/denli/main/.helix/headers.json" &&
+curl -X POST "https://admin.hlx.page/preview/dli3/denli/main/docs/library/library.json" &&
+curl -X POST "https://admin.hlx.page/preview/dli3/denli/main/metadata.json" &&
+curl -X POST "https://admin.hlx.page/preview/dli3/denli/main/fragments/gnav" &&
+curl -X POST "https://admin.hlx.page/preview/dli3/denli/main/fragments/footer" &&
+curl -X POST "https://admin.hlx.page/preview/dli3/denli/main/fragments/demo" &&
+curl -X POST "https://admin.hlx.page/preview/dli3/denli/main/fragments/404" &&
+curl -X POST "https://admin.hlx.page/live/dli3/denli/main/index" &&
+curl -X POST "https://admin.hlx.page/live/dli3/denli/main/placeholders.json" &&
+curl -X POST "https://admin.hlx.page/live/dli3/denli/main/.helix/headers.json" &&
+curl -X POST "https://admin.hlx.page/live/dli3/denli/main/docs/library/library.json" &&
+curl -X POST "https://admin.hlx.page/live/dli3/denli/main/metadata.json" &&
+curl -X POST "https://admin.hlx.page/live/dli3/denli/main/fragments/gnav" &&
+curl -X POST "https://admin.hlx.page/live/dli3/denli/main/fragments/footer" &&
+curl -X POST "https://admin.hlx.page/live/dli3/denli/main/fragments/demo" &&
+curl -X POST "https://admin.hlx.page/live/dli3/denli/main/fragments/404"
